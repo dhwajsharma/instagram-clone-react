257461e1e057b06338d667697b4c93fd644ae540
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-02 20:58:27 +0530

    Integrated modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
 import { makeStyles, Modal } from '@material-ui/core';
+import { Input } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Post from './components/Post/Post';
@@ -28,14 +29,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-
 function App() {
-
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle());
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
 
   useEffect(() => {
     db.collection("posts").onSnapshot(snapshot => {
@@ -54,7 +56,30 @@ function App() {
         onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <h1>Modal</h1>
+          <form className="app__signup">
+            <center>
+              <img className="app__headerImage" src="https://www.freepnglogos.com/uploads/instagram-logo-png-transparent-0.png" alt="" />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button>Sign Up</Button>
+          </form>
         </div>
       </Modal>
 
